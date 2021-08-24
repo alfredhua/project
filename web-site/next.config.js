043b77aa76601ce4ba42c16e74b1384903268958
huazhenguo <hua_zhenguo@163.com>
2021-08-24 08:12:03 +0800

    next

--- web-site/next.config.js
+++ web-site/next.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/website/home',
-          permanent: false,
-        },
-      ]
-    },
+    // async redirects() {
+    //   return [
+    //     {
+    //       source: '/',
+    //       destination: '/website/home',
+    //       permanent: false,
+    //     },
+    //   ]
+    // },
   }
