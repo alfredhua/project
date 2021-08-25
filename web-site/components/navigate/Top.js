367f83a2634e00e965bf26d127b174f47faaf059
guozhenhua7 <guozhenhua7@jd.com>
2021-08-25 19:02:37 +0800

    css

--- web-site/components/navigate/Top.js
+++ web-site/components/navigate/Top.js
@@ -2,7 +2,17 @@ import React from 'react';
 import classNames  from 'classnames';
 import styles from '@css/navigate/top.module.scss';
 
+var icons={
+  "google":"google"
+}
+
+
 export default class Top extends React.Component{
+
+  get_search_icon = ()=>{
+    return <div></div>
+  }
+
   render(){
     return (
             <div className={styles.container}>
@@ -32,7 +42,12 @@ export default class Top extends React.Component{
                     <div className={classNames(styles.dropdown,styles.settings)}>
                       <span className={styles.menu_name}><a href="/blog/other/ms">网站建设</a></span>
                     </div> 
-
+                </div>
+                <div className={styles.search}>
+                  <div className={styles.input}>
+                    {this.get_search_icon()}
+                    <input type='text'></input>
+                  </div>
                 </div>
              </div>
     )
