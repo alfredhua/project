5c87bece88887de16d3b9cf9644fa4362e4bb8d3
guozhenhua7 <guozhenhua7@jd.com>
2022-02-10 18:48:06 +0800

    dubbo

--- server/pro-server/server-auth/src/main/java/com/auth/AuthCore.java
+++ server/pro-server/server-auth/src/main/java/com/auth/AuthCore.java
@@ -2,12 +2,10 @@ package com.auth;
 
 import com.common.CommonCore;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 
-@MapperScan(basePackages = { "com.auth.dao"})
 @EnableDubbo(scanBasePackages = {"com.auth.rpc"})
 @Import({ CommonCore.class})
 @SpringBootApplication
