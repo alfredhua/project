795d76689172c7194086aba045b2604852531431
guozhenhua7 <guozhenhua7@jd.com>
2022-02-11 16:56:47 +0800

    zk

--- server/pro-common/common-spring-boot-starter/src/main/java/com/common/CommonCore.java
+++ server/pro-common/common-spring-boot-starter/src/main/java/com/common/CommonCore.java
@@ -1,6 +1,8 @@
 package com.common;
 
+import com.common.middle.zk.ZkUtil;
 import com.common.util.EnvUtil;
+import org.apache.curator.framework.CuratorFramework;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,9 +17,13 @@ public class CommonCore {
     @Autowired
     Environment environment;
 
+    @Autowired
+    CuratorFramework curatorFramework;
+
     @PostConstruct
     public void initEnvironment(){
         EnvUtil.initEnv(environment);
+        ZkUtil.initCuratorFramework(curatorFramework);
     }
 
 }
