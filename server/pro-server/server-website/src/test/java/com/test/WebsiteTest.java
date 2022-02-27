1c678522d301faff287ea4b4605732287863de7e
huazhenguo <hua_zhenguo@163.com>
2022-02-27 22:04:56 +0800

    websiteTest

--- server/pro-server/server-website/src/test/java/com/test/WebsiteTest.java
+++ server/pro-server/server-website/src/test/java/com/test/WebsiteTest.java
@@ -1,8 +1,7 @@
 package com.test;
 
 import com.WebsiteCore;
-import com.pro.blog.dto.entity.Article;
-import com.blog.service.ArticleService;
+import com.blog.constant.NavigateOneTypeEnum;
 import com.pro.website.dto.entity.Navigate;
 import com.website.service.NavigateService;
 import org.junit.Test;
@@ -14,7 +13,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author guozhenhua
@@ -25,30 +27,42 @@ import java.util.List;
 @ContextConfiguration(classes = WebsiteCore.class)
 public class WebsiteTest {
 
-
-    @Autowired
-    ArticleService articleService;
-
-    @Test
-    public void test() throws Exception {
-        List<Article> a=articleService.listArticleHome();
-        System.out.println(a);
+    static Map<NavigateOneTypeEnum,String> map=new HashMap<>();
+    static {
+        map.put(NavigateOneTypeEnum.BOOK,"book.txt");
+        map.put(NavigateOneTypeEnum.INDEX,"index.txt");
+        map.put(NavigateOneTypeEnum.MANUAL,"manual.txt");
     }
-
-
-
     @Autowired
     NavigateService navigateService;
 
     @Test
-    public void readFile(){
-        try {
-            String path = Thread.currentThread().getContextClassLoader().getResource("manual.txt").getPath();
-            FileReader fr = new FileReader(path);
-            BufferedReader br=new BufferedReader(fr);
-            String line;
-            while ((line=br.readLine())!=null){
-                String[] split =line.split("\\|");
+    public void insert(){
+        List<String> list = readFile(NavigateOneTypeEnum.MANUAL);
+        list.forEach(line->{
+            Navigate navigate=new Navigate();
+            String[] split =line.split("\\|");
+            navigate.setOne_type(NavigateOneTypeEnum.BOOK.getType());
+            navigate.setTwo_type(split[1]);
+            navigate.setIcon(split[2]);
+            navigate.setHref(split[3]);
+            navigate.setTitle(split[4]);
+            String introduce=null;
+            try{
+                introduce=split[5];
+            }catch (Exception e){
+                introduce=null;
+            }
+            navigate.setIntroduce(introduce);
+            navigateService.createNavigate(navigate);
+        });
+    }
+
+    @Test
+    public void update(){
+        List<String> list = readFile(NavigateOneTypeEnum.BOOK);
+        list.forEach(line->{
+            String[] split =line.split("\\|");
                 Navigate navigate=new Navigate();
                 navigate.setTitle(split[4]);
                 String introduce=null;
@@ -59,25 +73,25 @@ public class WebsiteTest {
                 }
                 navigate.setIntroduce(introduce);
                 navigateService.updateNavigate(navigate);
-//                navigate.setOne_type(NavigateOneTypeEnum.BOOK.getType());
-//                navigate.setTwo_type(split[1]);
-//                navigate.setIcon(split[2]);
-//                navigate.setHref(split[3]);
-//                navigate.setTitle(split[4]);
-//                String introduce=null;
-//                try{
-//                    introduce=split[5];
-//                }catch (Exception e){
-//                    introduce=null;
-//                }
-//                navigate.setIntroduce(introduce);
-//                navigateService.createNavigate(navigate);
+        });
+    }
+
+    private List<String> readFile(NavigateOneTypeEnum navigateOneTypeEnum){
+        List<String> list=new ArrayList<>();
+        try {
+            String path = Thread.currentThread().getContextClassLoader().getResource(map.get(navigateOneTypeEnum)).getPath();
+            FileReader fr = new FileReader(path);
+            BufferedReader br=new BufferedReader(fr);
+            String line;
+            while ((line=br.readLine())!=null){
+                list.add(line);
             }
             br.close();
             fr.close();
         }catch (Exception e){
             e.printStackTrace();
         }
+        return list;
     }
 
 }
