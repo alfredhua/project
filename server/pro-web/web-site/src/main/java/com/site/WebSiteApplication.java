7aee56b8d9b6625f83fce5027c9d1e6f24a90703
huazhenguo <hua_zhenguo@163.com>
2022-02-27 20:53:06 +0800

    blog

--- server/pro-web/web-site/src/main/java/com/site/WebSiteApplication.java
+++ server/pro-web/web-site/src/main/java/com/site/WebSiteApplication.java
@@ -1,13 +1,18 @@
-package com;
+package com.site;
 
+import com.WebsiteCore;
+import com.auth.AuthCore;
 import com.common.util.LogUtil;
+import com.message.MessageCore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 /**
  * @author hua
  */
 @SpringBootApplication
+@Import({AuthCore.class, MessageCore.class, WebsiteCore.class})
 public class  WebSiteApplication {
 
 
