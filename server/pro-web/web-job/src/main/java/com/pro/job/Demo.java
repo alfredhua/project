62aa37c839b5bbd4b9a7b3a0c759ca8e26c2e3fd
guozhenhua7 <guozhenhua7@jd.com>
2022-04-26 21:34:34 +0800

    demo

--- server/pro-web/web-job/src/main/java/com/pro/job/Demo.java
+++ server/pro-web/web-job/src/main/java/com/pro/job/Demo.java
@@ -1,18 +1,17 @@
 package com.pro.job;
 
 import com.common.util.LogUtil;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 /**
  * @auth guozhenhua
  * @date 2018/12/17
  */
-//@Component
+@Component
 public class Demo {
 
-
-    private final static String TIME =  "0/1 * * * * ?";
-
-//    @Scheduled(cron=TIME)
+    @Scheduled(cron="0/1 * * * * ?")
     public void cronJob(){
         LogUtil.info(" >>cron执行....");
     }
