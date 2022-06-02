745997f91cf76925cd729b26741e5c2d81836dc7
huazhenguo <hua_zhenguo@163.com>
2022-06-02 22:54:40 +0800

    admin

--- web-admin/src/auth/admin/AdminEdit.js
+++ web-admin/src/auth/admin/AdminEdit.js
@@ -45,9 +45,9 @@ export default class AdminEdit extends React.Component{
     admin.role_id_list=JSON.stringify(admin.role_id_list);
     admin.status=admin.status?1:0;
     if(admin.id){
-       result=await post("/auth/update-admin",{...admin});
+      result=await post("/auth/update-admin",{...admin});
     }else{
-       result= await post("/auth/create-admin",{...admin});
+      result= await post("/auth/create-admin",{...admin});
     }
     if(result && result.code === 'SUCCESS' ){
       message.success("保存成功",1,()=>{this.props.history.push(`/auth/list-admin`)});
@@ -56,19 +56,18 @@ export default class AdminEdit extends React.Component{
     }
   }
 
-
   render(){
-     return(
+    return(
       <div>
         <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>权限管理</Breadcrumb.Item>
-            <Breadcrumb.Item> <Link to="/auth/list-admin">管理员列表</Link></Breadcrumb.Item>
-            <Breadcrumb.Item>管理员编辑</Breadcrumb.Item>
+          <Breadcrumb.Item>权限管理</Breadcrumb.Item>
+          <Breadcrumb.Item> <Link to="/auth/list-admin">管理员列表</Link></Breadcrumb.Item>
+          <Breadcrumb.Item>管理员编辑</Breadcrumb.Item>
         </Breadcrumb>
         <Card>
-            <AdminEditForm {...this.state.admin} role_list={this.state.role_list} auth_data={this.state.auth_data} click={(values)=>{this.save(values)}} /> 
+          <AdminEditForm {...this.state.admin} role_list={this.state.role_list} auth_data={this.state.auth_data} click={(values)=>{this.save(values)}} /> 
         </Card>     
       </div>
-      );
-    }
+    );
+  }
 }
